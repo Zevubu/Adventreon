32d7999167d8287096b0a04e8e1c59394ca27e46
atocchi <andrewtocchi@gmail.com>
2020-03-18 20:10:19 -0700

    Reworking Nav Bar with Material UI

--- client/src/componets/navbar/index.js
+++ client/src/componets/navbar/index.js
@@ -1,12 +1,26 @@
 import React, {useState} from "react";
-import {Link} from 'react-router-dom'
-import {H2} from '../../styles/homeStyle'
-import {NavBody,  NavBtnB, NavBtnM, OptionBox} from "../../styles/componentStyles"
-import { useAuth, useUser, useManagment, useUserInfo } from "../../context/heart"
+import {Link} from 'react-router-dom';
+import {H2} from '../../styles/homeStyle';
+import {NavBody,  NavBtnB, NavBtnM, OptionBox, Logo} from "../../styles/componentStyles";
+import { useAuth, useUser, useManagment, useUserInfo } from "../../context/heart";
+import SimpleMenu from './menu.js';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
 // import { UserInfoContext } from "../../App"
+
+const useStyles = makeStyles(theme => ({
+    root: {
+      '& > *': {
+        margin: theme.spacing(1),
+      },
+    },
+  }));
+  
 
 
 function NavBar (props){
+    const classes = useStyles();
     // const [userProps, setUserProps] = useState(props)
     console.log(`navbar data: ${JSON.stringify(props)}`)
     const { setAuthTokens, isAuthenticated, setIsAuthenticated} = useAuth();
@@ -29,21 +43,24 @@ function NavBar (props){
     }
 
     return(
-        <NavBody>
+        
+        <NavBody  className={classes.root}>
             <OptionBox>
                 <OptionBox>
-                    <H2>Shtikr</H2>
+                    <Logo>Cantina</Logo>
                 </OptionBox>
                 
             </OptionBox>
             <OptionBox>
                 <OptionBox>
-                    <Link to="/"><NavBtnB>Home</NavBtnB></Link>
-                    <Link to="/events"><NavBtnB>events</NavBtnB></Link>
-                    <Link to="/schedule"><NavBtnB>Schedule</NavBtnB></Link>
+                    <Link to="/"><Button variant="outlined" color="secondary">Home</Button></Link>
+                    <br></br>
+                    <Link to="/events"><SimpleMenu></SimpleMenu></Link>
+                    <br></br>
+                    <Link to="/schedule"><Button variant="outlined" color="secondary">Schedule</Button></Link>
                     {isAuthenticated && isUser && (
                         <div>
-                            <Link to="/profile"><NavBtnB>Profile</NavBtnB></Link>
+                            <Link to="/profile"><Button variant="contained" color="secondary">Profile</Button></Link>
                         </div>
                     )}
                     
@@ -51,8 +68,8 @@ function NavBar (props){
                 <OptionBox>      
                     {!isAuthenticated && (
                         <div>
-                            <Link className="nav-link" to="/signup"><NavBtnM>Sign up</NavBtnM></Link>
-                            <Link className="nav-link" to="/login"><NavBtnM>Login</NavBtnM></Link>
+                            <Link className="nav-link" to="/signup"><Button variant="contained" color="secondary">Sign up</Button></Link>
+                            <Link className="nav-link" to="/login"><Button variant="contained" color="secondary">Login</Button></Link>
                         </div>
                     )}  
                     {isAuthenticated && (
@@ -62,6 +79,7 @@ function NavBar (props){
                 </OptionBox>
             </OptionBox>
         </NavBody>
+    
     )
 }
 
