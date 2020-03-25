918a007b8af4d4630f4411d436b7350644d6eb62
Zev Hoffman <zevubu@gmail.com>
2020-03-24 21:51:10 -0700

    heroku launch

--- client/src/pages/profiles/host.js
+++ client/src/pages/profiles/host.js
@@ -74,7 +74,12 @@ function Host(){
                         <ProTextBox>
                             <H3>Tip Me Here.</H3>
                             <br/>
-                            <P>{Host.payment}</P>
+                            <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                                <input type="hidden" name="cmd" value="_s-xclick"/>
+                                <input type="hidden" name="hosted_button_id" value="92TZKHPM5****"/>
+                                <input type="image" src="https://www.paypalobjects.com/en_US/GB/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal – The safer, easier way to pay online."/>
+                                <img alt="" border="0" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" width="1" height="1"/>
+                            </form>
                         </ProTextBox>
                         <ProTextBox>
                             <H3>My patreon.</H3>
