699b6d2c901c1736fa08d955b115c47105885639
Zev Hoffman <zevubu@gmail.com>
2020-03-25 04:25:49 -0700

    one more time

--- controllers/connection.js
+++ controllers/connection.js
@@ -1,16 +1,16 @@
 require("dotenv").config();
 const mysql = require('mysql');
 
-let connection
+// let connection
 module.exports = async (params) => new Promise(
     
     (resolve, reject) => {
-        if (process.env.JAWSDB_URL) {
+        // if (process.env.JAWSDB_URL) {
             // Database is JawsDB on Heroku
-            connection = mysql.createConnection(process.env.JAWSDB_URL);
-        } else {
-        connection = mysql.createConnection(params);
-        }
+           const connection = mysql.createConnection(process.env.JAWSDB_URL);
+        // } else {
+        // connection = mysql.createConnection(params);
+        // }
         connection.connect(error => {
             if (error) {
                 console.log(error)
