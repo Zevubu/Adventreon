157fe8e91d2ba2a9bcafbdf2a8583dd06360f394
Zev Hoffman <zevubu@gmail.com>
2020-03-25 02:30:20 -0700

    heroku push again

--- controllers/connection.js
+++ controllers/connection.js
@@ -5,12 +5,12 @@ const keys = require("../keys");
 module.exports = async (params) => new Promise(
     
     (resolve, reject) => {
-        // if (process.env.JAWSDB_URL) {
-        //     // Database is JawsDB on Heroku
-        //     connection = mysql.createConnection(process.env.JAWSDB_URL);
-        // } else {
+        if (process.env.JAWSDB_URL) {
+            // Database is JawsDB on Heroku
+            connection = mysql.createConnection(process.env.JAWSDB_URL);
+        } else {
         const connection = mysql.createConnection(params);
-        // }
+        }
         connection.connect(error => {
             if (error) {
                 console.log(error)
