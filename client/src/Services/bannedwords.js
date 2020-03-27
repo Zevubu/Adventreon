a41d074016873b3f3216d8de554b70129f1aad96
atocchi <andrewtocchi@gmail.com>
2020-03-26 22:40:17 -0700

    added reverse array to chat, starting on bad word filter

---
+++ client/src/Services/bannedwords.js
@@ -0,0 +1,4 @@
+//Trigger Warning for Offensive Content, this is a list of Vile words
+// that we need to compile so that we can ban them from our platform
+
+export default words = ['poopy','stupid', 'dumb']
