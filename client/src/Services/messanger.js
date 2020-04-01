e168ab7eea91694386a30bf0060f5aafb2a1bd9c
atocchi <andrewtocchi@gmail.com>
2020-04-01 09:57:28 -0700

    adding proto-type auth to chat

--- client/src/Services/messanger.js
+++ client/src/Services/messanger.js
@@ -4,7 +4,8 @@ import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import wordFilter from './wordFilter'
-
+import { auth } from '../Services/firebase';
+import { signInWithGoogle } from '../Services/Signin'
 
 ///Messenger takes in room={'foo'} as a prop
 //this determines the room that post will be sent too
@@ -19,6 +20,10 @@ const [chat, setChat] = useState ({
     readError: null,
     writeError: null,
 });
+
+const [user, setUser] = useState ({
+  user: auth().currentUser
+})
 
 Messenger.defaultProps = {
  isMod: false,
@@ -38,30 +43,56 @@ const [text, setText] = useState({
 }
 )
 
+async function googleSignIn() {
+  try {
+    await signInWithGoogle();
+  } catch (error) {
+    setChat({ error: error.message });
+  }
+}
+
+function signOut () {
+auth().signOut().then(function() {
+  console.log('Signed Out');
+}, function(error) {
+  console.error('Sign Out Error', error);
+});
+}
+
+
+
+
+
+
+
 function handleChange(event) {
    setText({
       content: event.target.value
     });
-}
+}   
 
     async function handleSubmit(event) {
       if(text.content === ''){
+        event.preventDefault();
+        console.log(auth().currentUser)
         alert('You cannot send an empty mesage!')
       }
-      else if(name.name === '' || name.name === null){
+      else if(auth().currentUser === null){
         event.preventDefault();
-        let nName = prompt("Please enter in a chat name",`Anon${(Math.random() * 999)}`);
-        setName({name: nName})
+        alert("Please use the button to log into Google below!")
       }
-      else if(name.name !== ''){
+      else if(auth().currentUser !== null){
         event.preventDefault();
+        console.log(auth().currentUser)
         setChat({ writeError: null });
         try {
           await db.ref(props.room).push({
             content: text.content,
             timestamp: Date(Date.now()),
             name: name.name,
-            mod: props.isMod
+            mod: props.isMod,
+            uid: auth().currentUser.uid,
+            email: auth().currentUser.email
           });
           setText({ content: '' });
         } catch (error) {
@@ -73,7 +104,7 @@ function handleChange(event) {
 
 
 useEffect(() =>{
-  // console.log("pulling Ref " + props.room)
+    setUser({user: auth().currentUser})
     db.ref(props.room).on("value", snapshot => {
         let adenvtreon = [];
         snapshot.forEach((snap) => {
@@ -89,16 +120,16 @@ return (
       <Paper style={matches ? {backgroundColor: 'grey', width: '520px', padding: '20px', height: '200px', overflow: 'auto'} :{backgroundColor: 'grey', width: '335px', padding: '20px', height: '300px', overflow: 'auto'} }>
       <div className={props.room}>
         {chat.adenvtreon.map(chat => {
-        if(chat.name === name.name){
+        if(chat.email === auth().currentUser.email){
           return <Paper elevation={2} style={chat.mod ? {fontFamily: 'Baloo 2, cursive', width:'50%',padding:'5px', paddingBottom: '15px', marginBottom: '15px',backgroundColor: '#FDFD96'} : {fontFamily: 'Baloo 2, cursive', width:'50%',padding:'5px', paddingBottom: '15px', marginBottom: '15px',backgroundColor: '#add8e6'}} key={chat.timestamp}>{props.child ? wordFilter(chat.content) : chat.content} 
           <br/>
-          <p style={{fontSize: '5px'}}>{chat.name}~ @{chat.timestamp}</p>
+          <p style={{fontSize: '5px'}}>{chat.email}~ @{chat.timestamp}</p>
           </Paper>
         }
         else{
           return <Paper elevation={2} style={chat.mod ?{fontFamily: 'Baloo 2, cursive', width:'50%',padding:'5px', paddingBottom: '15px', marginBottom: '15px', marginLeft: '30px',backgroundColor: '#FDFD96' } : {fontFamily: 'Baloo 2, cursive', width:'50%',padding:'5px', paddingBottom: '15px', marginBottom: '15px', marginLeft: '30px'}} key={chat.timestamp}>{props.child ? wordFilter(chat.content) : chat.content}
           <br/>
-          <p style={{fontSize: '5px'}}>{chat.name}~ @{chat.timestamp}</p>
+          <p style={{fontSize: '5px'}}>{chat.email}~ @{chat.timestamp}</p>
           </Paper>
         }
         })}
@@ -108,6 +139,12 @@ return (
         <input onChange={handleChange} style={matches ? {width: '440px', marginBottom: '20px', padding: '20px'} : {width: '257px', marginBottom: '20px', padding: '20px'}} value={text.content}></input>
         <button style={{padding: '20px'}} type="submit">Send</button>
       </form>
+      <button onClick={googleSignIn} type="button">
+      Sign up with Google
+      </button>
+      <button onClick={signOut} type="button">
+        Sign out
+      </button>
     </div>
   );
 
